6668d1ac7a6cd62740200dac41e22f3828bbb3b8
Yusuf <y.adanur@gmail.com>
2020-08-04 23:18:09 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
+import 'jquery/dist/jquery.js';
+import 'popper.js/dist/umd/popper.js';
+import 'bootstrap/dist/js/bootstrap.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
