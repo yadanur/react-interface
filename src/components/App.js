2cba75617209d6d1a821084aff01ec7019931ebd
Yusuf <y.adanur@gmail.com>
2020-08-05 23:45:40 -0700

    start setup working

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import '../css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <main className="page bg-white" id="petratings">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 bg-white">
+            <div className="container">
+              <div>Add Appointments</div>
+              <div>Search Appointments</div>
+              <div>List Appointments</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </main>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-/*
-import 'bootstrap/dist/css/ucbootstrap.css';
-import './index.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './css/index.css';
 import App from './components/App.js';
 import 'jquery/dist/jquery.js';
 import 'popper.js/dist/umd/popper.js';
 import 'bootstrap/dist/js/bootstrap.js';
 import * as serviceWorker from './serviceWorker';
-*/
+
 
 function MyApp (){
   return(
@@ -24,6 +24,8 @@ function MyApp (){
 
 ReactDOM.render(
   
-    <MyApp />,
+    <App />,
   document.getElementById('root')
 );
+
+serviceWorker.unregister();
