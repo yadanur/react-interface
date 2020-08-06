cfc5d49e8f549e6061cee87667317d1f28dd7b67
Yusuf <y.adanur@gmail.com>
2020-08-05 22:58:05 -0700

    a working version

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+/*
+import 'bootstrap/dist/css/ucbootstrap.css';
 import './index.css';
-import App from './App';
+import App from './components/App.js';
 import 'jquery/dist/jquery.js';
 import 'popper.js/dist/umd/popper.js';
 import 'bootstrap/dist/js/bootstrap.js';
 import * as serviceWorker from './serviceWorker';
+*/
+
+function MyApp (){
+  return(
+    <>
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    </>
+  );
+
+
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <MyApp />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
