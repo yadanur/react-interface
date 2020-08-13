628041b97f8f27cd1d48d6dddfe16a36aefdd1e4
Yusuf <y.adanur@gmail.com>
2020-08-13 01:06:34 -0700

    Form added - add appointment form

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ class App extends Component {
     super(); //super lets you use this statement
     this.state={
       myAppointments: [],
+      formDisplay:false,
       lastIndex:0
     }
     this.deleteAppointment=this.deleteAppointment.bind(this);
@@ -51,7 +52,7 @@ class App extends Component {
         <div className="row">
           <div className="col-md-12 bg-white">
             <div className="container">
-              <AddAppointments />
+              <AddAppointments formDisplay={this.state.formDisplay} />
               <SearchAppointments />
               <ListAppointments appointments={this.state.myAppointments}
               deleteAppointment={this.deleteAppointment}/>
