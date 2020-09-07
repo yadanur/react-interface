5883cd6003aee049ab360fc02cf6a683eea0978a
Yusuf <y.adanur@gmail.com>
2020-09-07 00:24:34 -0700

    toggle feature added

--- src/components/AddAppointments.js
+++ src/components/AddAppointments.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'; //imports component from node-module folder
+import {FaPlus} from 'react-icons/fa';
 
 class AddAppointments extends Component {
     render(){
@@ -6,8 +7,11 @@ class AddAppointments extends Component {
             <div className= {'card textcenter mt-3' +
              (this.props.formDisplay ? '' : ' add-appointment') 
             }>
-            <div className="apt-addheading card-header bg-primary text-white">
-              Add Appointment
+            <div 
+             className="apt-addheading card-header bg-primary text-white"
+             onClick = {this.props.toggleForm}
+            >
+              <FaPlus /> Add Appointment
             </div>
   
             <div className="card-body">
