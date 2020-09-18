604192f338a1df6c0c3b41225ec46f88382a276a
Yusuf <y.adanur@gmail.com>
2020-09-17 23:02:25 -0700

    sort data feature added

--- src/components/App.js
+++ src/components/App.js
@@ -13,16 +13,27 @@ class App extends Component {
     this.state={
       myAppointments: [],
       formDisplay:false,
+      orderDir:'asc',
+      orderBy:'petName',
+
       lastIndex:0
     }
     this.deleteAppointment=this.deleteAppointment.bind(this);
     this.toggleForm = this.toggleForm.bind(this);
     this.addAppointment=this.addAppointment.bind(this);
+    this.changeOrder=this.changeOrder.bind(this);
   }
 
   toggleForm(){
     this.setState({
       formDisplay: !this.state.formDisplay
+    });
+  }
+
+  changeOrder(order,dir){
+    this.setState({
+      orderBy: order,
+      orderDir:dir
     });
   }
 
@@ -64,6 +75,22 @@ class App extends Component {
   }
   
   render(){
+    let order;
+    let filteredApts=this.state.myAppointments;
+    if (this.state.orderDir==='asc'){
+      order=1;
+    } else {
+      order= -1;
+    }
+
+    filteredApts.sort((a,b) => {
+      if (a[this.state.orderBy].toLowerCase() < b[this.state.orderBy].toLowerCase() ) {
+        return -1 * order;
+      } else {
+        return 1 * order;
+      }
+    });
+
     return (
       <main className="page bg-white" id="petratings">
       <div className="container">
@@ -75,8 +102,12 @@ class App extends Component {
               toggleForm={this.toggleForm} 
               addAppointment={this.addAppointment}
               />
-              <SearchAppointments />
-              <ListAppointments appointments={this.state.myAppointments}
+              <SearchAppointments 
+                orderBy={this.state.orderBy}
+                orderDir={this.state.orderDir}
+                changeOrder={this.changeOrder}
+              />
+              <ListAppointments appointments={filteredApts}
               deleteAppointment={this.deleteAppointment}/>
             </div>
           </div>
