e4f6dc1ceafd040e8b4a942355c0356f7a4f4346
Yusuf <y.adanur@gmail.com>
2020-09-19 22:04:10 -0700

    Updates

---
+++ precache-manifest.c1c2c9d00238029af7571e80bc85a1b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba02aaa448be6c5ab7f9b4747ed2cce6",
+    "url": "/react-interface/index.html"
+  },
+  {
+    "revision": "01931e6426de1b9219af",
+    "url": "/react-interface/static/css/2.6e90b12a.chunk.css"
+  },
+  {
+    "revision": "b9fcd18261df7d8ae0ad",
+    "url": "/react-interface/static/css/main.113080c1.chunk.css"
+  },
+  {
+    "revision": "01931e6426de1b9219af",
+    "url": "/react-interface/static/js/2.5f0c5a14.chunk.js"
+  },
+  {
+    "revision": "642e7517a5e9c420d9bec80a5b09d8df",
+    "url": "/react-interface/static/js/2.5f0c5a14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9fcd18261df7d8ae0ad",
+    "url": "/react-interface/static/js/main.28ead7f2.chunk.js"
+  },
+  {
+    "revision": "96414c0fd7bffbe37649",
+    "url": "/react-interface/static/js/runtime-main.543a354a.js"
+  }
+]);
